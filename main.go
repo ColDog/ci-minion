7335f5db2cf06cd2f94a4dcaa051f1313ced0552
Colin Walker <colin@musefind.com>
2016-07-09 14:18:08 -0700

    add current-state endpoint for viewing commands

--- main.go
+++ main.go
@@ -1,27 +1,9 @@
 package main
 
-import "time"
-
 const LOG_OUTPUT bool = false
 
 
 func main()  {
-	//minion := NewMinion()
-	//minion.Start()
-
-	repo := Repo{
-		Branch: "master",
-		Provider: "github",
-		Organization: "coldog",
-		Project: "ci-sample",
-	}
-
-	job := NewJob("test1", repo)
-
-	go func() {
-		time.Sleep(10 * time.Second)
-		job.Quit()
-	}()
-
-	job.Run()
+	minion := NewMinion()
+	minion.Start()
 }
--- server.go
+++ server.go
@@ -1,60 +1,72 @@
 package main
-//
-//import (
-//	"net/http"
-//	"fmt"
-//)
-//
-//type Minion struct {
-//	cancel 		chan bool
-//}
-//
-//func (server *Minion) handle(w http.ResponseWriter, r *http.Request) {
-//	server.cancel <- true
-//}
-//
-//func (server *Minion) serve()  {
-//	http.HandleFunc("/cancel", server.handle)
-//	http.ListenAndServe(":8000", nil)
-//}
-//
-//func (server *Minion) GetNextJob() *Job {
-//	repo := Repo{
-//		Branch: "master",
-//		Provider: "github",
-//		Organization: "coldog",
-//		Project: "ci-sample",
-//	}
-//
-//	job := NewJob("test1", repo)
-//
-//	return job
-//}
-//
-//func (server *Minion) run() {
-//	for {
-//		job := server.GetNextJob()
-//		go job.Run()
-//
-//		select {
-//		case job.finished:
-//			out := job.Serialize()
-//			fmt.Printf("\n%s\n", out)
-//			continue
-//
-//		case server.cancel:
-//			job.Quit()
-//		}
-//	}
-//}
-//
-//func (server *Minion) Start() {
-//	go server.serve()
-//	server.run()
-//}
-//
-//func NewMinion() *Minion {
-//	return Minion{
-//		cancel: make(chan bool),
-//	}
-//}
+
+import (
+	"net/http"
+	"fmt"
+)
+
+type Minion struct {
+	cancel 		chan bool
+	current		*Job
+}
+
+func (server *Minion) handleCancel(w http.ResponseWriter, r *http.Request) {
+	server.cancel <- true
+}
+
+func (server *Minion) viewJob(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(server.current.JobId))
+}
+
+func (server *Minion) viewCurrentState(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(server.current.Serialize()))
+}
+
+func (server *Minion) serve()  {
+	http.HandleFunc("/cancel", server.handleCancel)
+	http.HandleFunc("/current", server.viewJob)
+	http.HandleFunc("/current-state", server.viewCurrentState)
+	http.ListenAndServe(":8000", nil)
+}
+
+func (server *Minion) GetNextJob() *Job {
+	repo := Repo{
+		Branch: "master",
+		Provider: "github",
+		Organization: "coldog",
+		Project: "ci-sample",
+	}
+
+	job := NewJob("test1", repo)
+
+	return job
+}
+
+func (server *Minion) run() {
+	for {
+		server.current = server.GetNextJob()
+		go server.current.Run()
+
+		select {
+		case <- server.current.finished:
+			out := server.current.Serialize()
+			fmt.Printf("\n%s\n", out)
+			continue
+
+		case <- server.cancel:
+			server.current.Quit()
+		}
+	}
+}
+
+func (server *Minion) Start() {
+	go server.serve()
+	server.run()
+}
+
+func NewMinion() *Minion {
+	return &Minion{
+		cancel: make(chan bool),
+	}
+}
